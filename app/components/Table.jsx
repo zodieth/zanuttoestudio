30ae4af59a27092f5a1a47abf9e91a0538539481
zodieth <contact.prodzodi@gmail.com>
2023-07-19 12:29:29 -0300

    searchbar css

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -28,7 +28,26 @@ function Table({ people }) {
       )}
       {editUser && <EditUser user={user} setEditUser={setEditUser} />}
 
-      <SearchBar setSearch={setSearch} />
+      <div className="flex flex-row items-center justify-between mb-10">
+        <SearchBar setSearch={setSearch} />
+        <div className="mx-1 flex flex-row items-center justify-center rounded bg-blue-600 px-4 py-3 text-xs font-medium text-white hover:bg-blue-500 cursor-pointer">
+          <h1 className="text-md">Crear</h1>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-4 w-4 ml-2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+            />
+          </svg>
+        </div>
+      </div>
 
       {people.isLoading === true ? (
         <RiLoader5Fill
