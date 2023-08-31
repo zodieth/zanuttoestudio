e5fb4a53b923959d48356d94efacee1e59b82cd2
Mateo Iglesias <contact.prodzodi@gmail.com>
2023-08-31 12:25:02 -0300

    overflow-x-hidden

--- app/components/EditUser.jsx
+++ app/components/EditUser.jsx
@@ -292,7 +292,10 @@ function EditUser({ user, setEditUser }) {
                           className="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
                           defaultValue={usuario.hijos >= 0 ? usuario.hijos : 0}
                           onChange={(e) =>
-                            setUsuario({ ...usuario, hijos: e.target.value })
+                            setUsuario({
+                              ...usuario,
+                              hijos: e.target.value,
+                            })
                           }
                         />
                       </div>
@@ -887,6 +890,15 @@ function EditUser({ user, setEditUser }) {
                     onClick={async () => [
                       // setEditUser(false),
                       dispatch(changeLoading(true)),
+                      usuario.sexo === "MASCULINO"
+                        ? setUsuario({
+                            ...usuario,
+                            hijos: 0,
+                            hijosDiscapacidad: 0,
+                            hijosAdoptados: 0,
+                            auh: 0,
+                          })
+                        : "",
                       await updateUser(
                         usuario._id,
                         usuario.nombre,
@@ -916,6 +928,7 @@ function EditUser({ user, setEditUser }) {
                         usuario.provincia,
                         usuario.detalle
                       ),
+
                       createOrUpdateDetail(),
                       dispatch(editPerson(usuario)),
                       dispatch(changeLoading(false)),
