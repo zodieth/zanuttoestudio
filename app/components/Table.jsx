ec755b46a118cb3125d17cb28eb50649f82481e1
LucasParra95 <lucasandresparra@gmail.com>
2023-09-01 10:24:12 -0300

    Boton Exportar solo exporta los seleccionados, boton Borrar Seleccionados sin funcion

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -32,6 +32,34 @@ function Table({ people, detail }) {
   const [deleteUser, setDeleteUser] = useState(false);
   const [editUser, setEditUser] = useState(false);
 
+  const [checkedState, setCheckedState] = useState(
+    new Array(peoplePagination.length).fill(false)
+  )
+
+  const [allCheckedState, setAllCheckedState] = useState(false)
+  const handleSelectOne = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) => 
+    index===position ? !item : item
+    );
+    const allCheckedArr = checkedState.map(() => true)
+    updatedCheckedState === allCheckedArr ? setAllCheckedState(true) : setAllCheckedState(false)
+    setCheckedState(updatedCheckedState);
+  console.log(updatedCheckedState);
+  console.log(allCheckedArr);
+  //   checkedState === allChecked ? true : false;
+  }
+  const handleSelectAll = (e) => {
+    if(e.target.checked===true){
+      const allSelected = new Array(peoplePagination.length).fill(true);
+      setCheckedState(allSelected);
+      setAllCheckedState(true);
+    }else{
+      const noneSelected = new Array(peoplePagination.length).fill(false);
+      setCheckedState(noneSelected);
+      setAllCheckedState(false);
+    }
+  };
+
   const [user, setUser] = useState({
     nombr: "",
     sexo: "",
@@ -61,8 +89,18 @@ function Table({ people, detail }) {
   });
   const [search, setSearch] = useState("");
 
-  const dataPerson = people.people
-  const dataDetails = people.people.map((p) => {
+  const dataSelected = (people, checkedState) => {
+    const dataPersonArr = [];
+    people.map((item, index)=>{
+      if(checkedState[index]){
+        dataPersonArr.push(item)
+      }
+    });
+    return dataPersonArr;
+  }
+  const dataPerson = dataSelected(peoplePagination, checkedState)
+  //const dataPerson = people.people
+  const dataDetails = dataPerson.map((p) => {
     const d = detail.detail
     for(let i=0; i<d.length; i++){
       if(d[i].persona === p._id){
@@ -126,6 +164,25 @@ function Table({ people, detail }) {
             />
           </svg>
         </div>
+        <div className="mx-1 flex flex-row items-center justify-center rounded bg-red-600 px-4 py-3 text-xs font-medium text-white hover:bg-blue-500 cursor-pointer"
+          //onClick={()=>downloadExcel(dataPerson,dataDetails)}
+        >
+          <h1 className="text-md">Borrar Seleccionados</h1>
+             <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              />
+            </svg>
+        </div>
       </div>
 
       <div className="flex flex-row items-center justify-center">
@@ -162,7 +219,11 @@ function Table({ people, detail }) {
                   <th className="text-left whitespace-nowrap px-4 py-2 font-medium text-gray-900">
                     <div className="flex flex-col items-center justify-center">
                       Seleccionar todos
-                      <input type="checkbox" />
+                      <input type="checkbox" 
+                        //defaultChecked={allCheckedState}
+                        checked={allCheckedState}
+                        onChange={(e) => handleSelectAll(e)}
+                      />
                     </div>
                   </th>
                   <th className="px-4 py-2"></th>
@@ -221,7 +282,10 @@ function Table({ people, detail }) {
                         </a>
                       </td>
                       <td className="flex justify-center">
-                          <input type="checkbox" />
+                          <input type="checkbox"
+                            checked={checkedState[index]}
+                            onChange={() => handleSelectOne(index)}
+                          />
                       </td>
                       <td className="whitespace-nowrap px-4 py-2">
                         <div
