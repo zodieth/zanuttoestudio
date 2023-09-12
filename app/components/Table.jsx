78799d0070e1d8e0879fc7a504bae1c468d27aa1
LucasParra95 <lucasandresparra@gmail.com>
2023-09-12 15:39:37 -0300

    Privatizar Rutas API con filtrado por carpeta

--- app/components/Table.jsx
+++ app/components/Table.jsx
@@ -22,7 +22,32 @@ function Table() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    api.get("people").then((data) => dispatch(addPeople(data.data)));
+    api.get("people")
+    .then((datos) =>
+      datos.data.sort((a, b) => {
+        const idA = a.idInc || a._id;
+        const idB = b.idInc || b._id;
+
+        // Si idA es número y idB es string, a debe aparecer primero
+        if (typeof idA === "number" && typeof idB === "string") {
+          return -1;
+        }
+
+        // Si idA es string y idB es número, b debe aparecer primero
+        if (typeof idA === "string" && typeof idB === "number") {
+          return 1;
+        }
+
+        // Si ambos son números, ordenar de manera descendente
+        if (typeof idA === "number" && typeof idB === "number") {
+          return idB - idA;
+        }
+
+        // Si ambos son strings, se pueden comparar lexicográficamente (esto mantendrá el orden basado en el componente temporal de los ObjectIDs de MongoDB)
+        return idA.localeCompare(idB);
+      })
+    )
+    .then((data) => dispatch(addPeople(data)));
     api.get("detalle").then((data) => dispatch(addDetail(data.data)));
 
   }, [dispatch]);
