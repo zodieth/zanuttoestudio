9d21d15ba15dd559b2bcb156c54b918631fff98d
Lucas Parra <109368706+LucasParra95@users.noreply.github.com>
2023-09-19 11:25:03 -0300

    Update token

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function middleware( request ) {
-    const token = request.cookies.get("next-auth.session-token")
+    const token = request.cookies.get("_Secure-next-auth.session-token")
     if(request.nextUrl.pathname.includes('/api') && request.method === 'GET' && !request.nextUrl.pathname.includes('/api/auth')) {
         if(token === undefined) {
             return NextResponse.redirect(new URL('/oficina', request.url))
